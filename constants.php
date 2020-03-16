21799964f5bb713107fc17eb8185fd421c720760
destrotechs <morrisdestro@gmail.com>
2020-03-16 13:39:04 +0300

    Update constants.php

--- constants.php
+++ constants.php
@@ -1,17 +1,17 @@
 <?php
 
 define("DBHOST", "127.0.0.1");
-define("DBNAME", "radiusdb");
+define("DBNAME", "radius");
 define("DBPASSWORD","");
 define('DBUSER','root');
-define('CONSUMER_KEY', 'ZZo76VARLRb5rTuZRr2I6SgkzuAwGudn');
-define('CONSUMER_SECRET', 'olakpIBAWRfvTPGF');
-define('SHORT_CODE', '885755');
-define('PARTYB', '887803');
-define('CALLBACK_URL', 'https://hewanet.co.ke/callback/');
-define('VALIDATION_URL', 'https://hewanet.co.ke/validation');
-define('CONFIRMATION_URL','https://hewanet.co.ke/confirmation');
-define('ENV', 'api');
-define('passkey', '814187fb60546f1b54e3299f3f7ed7f1f13a304df893840ba2d67dbb9748d768');
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('SHORT_CODE', '');
+define('PARTYB', '');
+define('CALLBACK_URL', '');
+define('VALIDATION_URL', '');
+define('CONFIRMATION_URL','');
+define('ENV', 'sandbox');
+define('passkey', '');
 
 ?>
