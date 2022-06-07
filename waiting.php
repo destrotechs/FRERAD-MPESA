9008e9b75c39bf6b3c78b7ace2039c9eec0dc1bb
Morris Mbae #Destro <morrisdestro@gmail.com>
2022-06-07 10:11:09 +0300

    Update waiting.php

--- waiting.php
+++ waiting.php
@@ -5,3 +5,10 @@ echo("<div class='alert alert-primary'>Waiting for transaction to complete</div>
 
 include("footer.php");
 ?>
+//sudo apt install php7.0
+//sudo apt install php7.0-cgi
+//sudo apt install php7.0-gd
+//sudo apt install php7.0-mysql
+//sudo apt install mysql-server
+//sudo apt install phpmyadmin
+//sudo apt install libapache2-mod-php7.0
